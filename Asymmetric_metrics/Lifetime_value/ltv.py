9c51b34ab2a59f288400ee76b8796417938526a7
UssinaSabina <102852241+UssinaSabina@users.noreply.github.com>
2023-04-07 13:21:56 +0300

    Delete ltv.py

--- Asymmetric_metrics/Lifetime_value/ltv.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def ltv_error(y_true: np.array, y_pred: np.array) -> float:
-    residual = (y_pred - y_true).astype(float)
-    loss = np.where(residual > 0, 10 * residual ** 2, residual ** 2)
-    error = np.mean(loss)
-    return error
